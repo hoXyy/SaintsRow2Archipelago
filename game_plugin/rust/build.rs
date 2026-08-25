c5028af64a39ce40a358d387ab6fba58c1a4ee02
Paweł Zatoka <github@hoxi.me>
2026-08-25 03:51:35 +0200

    feat: small rust refactor
    the FFI declarations are split between modules now

--- game_plugin/rust/build.rs
+++ game_plugin/rust/build.rs
@@ -1,7 +1,7 @@
 fn main() {
     cxx_build::CFG.include_prefix = "sr2ap";
 
-    cxx_build::bridge("src/ffi.rs")
+    cxx_build::bridge("src/ffi/logger_ffi.rs")
         .std("c++17")
         .compile("sr2ap-cxxbridge");
 
